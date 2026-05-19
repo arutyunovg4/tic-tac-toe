dbc6c8f2a0cd9e8e27702b4bd7100e8a130eb30e
Georgiy Arutyunov <lvlygshn4@gmail.com>
2026-05-20 01:23:11 +0300

    created a structure plan

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static void startGameRound(){
+        //create board
+        //startGameLoop
+    }
+    public static void startGameLoop() {
+        //while (gameNotOver)
+        // playerTun
+        // botTurn
+        // checkGameState
+    }
+
+    public static void makePlayerTurn() {
+        // get input
+        //validate input
+        // place X on board
+    }
+
+    public static void makeBotTurn() {
+        // get random empty cell
+        // place 0 on board
+
+    }
+
+    public static void checkGameState(){
+        // x = 1, 0 - (-1), empty - 0
+        // count sum for rows,columns and diagonals
+
+        // if sum.contains(3) -> x won
+        // if sum.contains(-3) -> 0 won
+        // if all cells occupied -> DRAW
+        // else game keep going
+    }
+}
