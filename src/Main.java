7cb1786b9d6570d9e3862ffa069853388c5cd8e7
Georgiy Arutyunov <lvlygshn4@gmail.com>
2026-05-20 16:45:28 +0300

    added method realisation for: createBoard() inputCellCoordinates()

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,80 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    private static final int ROW_COUNT = 3;
+    private static final int COL_COUNT = 3;
+    private static final String CELL_STATE_EMPTY = " ";
+    private static final String CELL_STATE_X = "X";
+    private static final String CELL_STATE_0 = "0";
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[][] board = createBoard();
+
+        board[0][0] = CELL_STATE_0;
+
+        inputCellCoordinates(board);
+
+        System.out.println("Hi");
     }
-    public static void startGameRound(){
+
+    public static void startGameRound() {
         //create board
         //startGameLoop
+        String[][] board = createBoard();
+        startGameLoop(board);
     }
-    public static void startGameLoop() {
+
+    public static String[][] createBoard() {
+        String[][] board = new String[ROW_COUNT][COL_COUNT];
+
+        for (int row = 0; row < ROW_COUNT; row++) {
+            for (int col = 0; col < COL_COUNT; col++) {
+                board[row][col] = CELL_STATE_EMPTY;
+            }
+        }
+        return board;
+    }
+
+    public static void startGameLoop(String[][] board) {
         //while (gameNotOver)
         // playerTun
         // botTurn
         // checkGameState
     }
 
-    public static void makePlayerTurn() {
+    public static int[] inputCellCoordinates(String[][] board) {
+        System.out.println("Введите координаты через пробел от 0 до 2");
+
+
+        do {
+            try {
+                String[] input = scanner.nextLine().split(" ");
+
+            int row = Integer.parseInt(input[0]);
+            int col = Integer.parseInt(input[1]);
+            if (row >= ROW_COUNT || row < 0 || col >= COL_COUNT || col < 0) {
+                System.out.println("Значения должны быть от 0 до 2");
+            } else if (!board[row][col].equals(CELL_STATE_EMPTY)) {
+                System.out.println("Клетка занята");
+            }
+            else{
+                return new int[]{row, col};
+            }
+
+            }catch (RuntimeException e){
+                System.out.println("Значения должны быть от 0 до 2 и написаны через пробел");
+            }
+
+        } while (true);
+    }
+
+    public static void makePlayerTurn(String[][] board) {
         // get input
-        //validate input
+        int[] coordinates = inputCellCoordinates(board);
         // place X on board
+        board[coordinates[0]][coordinates[1]] = CELL_STATE_X;
+
     }
 
     public static void makeBotTurn() {
@@ -25,7 +83,7 @@ public class Main {
 
     }
 
-    public static void checkGameState(){
+    public static void checkGameState() {
         // x = 1, 0 - (-1), empty - 0
         // count sum for rows,columns and diagonals
 
